5ce5f6b854967af0b3c05d5d60315a90a315f222
li-chunlu2016 <David@dhcp-10-105-124-110.wireless.northwestern.private>
2016-07-14 12:07:32 -0500

    TPadLibraryV1 upload

---
+++ TPadLibraryV1/openCVLibrary310/build/intermediates/ndk/release/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := openCVLibrary310
+LOCAL_LDFLAGS := -Wl,--build-id
+LOCAL_SRC_FILES := \
+	/Users/David/Documents/AndroidProjects/TPadLibraryV1/openCVLibrary310/src/main/jniLibs/armeabi-v7a/libopencv_java3.so \
+	/Users/David/Documents/AndroidProjects/TPadLibraryV1/openCVLibrary310/src/main/jniLibs/x86/libopencv_java3.so \
+	/Users/David/Documents/AndroidProjects/TPadLibraryV1/openCVLibrary310/src/main/jniLibs/x86_64/libopencv_java3.so \
+
+LOCAL_C_INCLUDES += /Users/David/Documents/AndroidProjects/TPadLibraryV1/openCVLibrary310/src/main/jni
+LOCAL_C_INCLUDES += /Users/David/Documents/AndroidProjects/TPadLibraryV1/openCVLibrary310/src/main/jniLibs
+LOCAL_C_INCLUDES += /Users/David/Documents/AndroidProjects/TPadLibraryV1/openCVLibrary310/src/release/jni
+
+include $(BUILD_SHARED_LIBRARY)
